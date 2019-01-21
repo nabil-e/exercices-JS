3bdbd4a64fb322a15eb408aacd0cf03e29263192
Nabil-e <nabilelassal@gmail.com>
2019-01-21 16:18:06 +0100

    Troisième série exercices

---
+++ ex3_1.js
@@ -0,0 +1,5 @@
+let arr = [100, 101, 102]
+let sum = 0
+for (i of arr){
+    sum += parseInt(i)
+}console.log(sum)
---
+++ ex3_3a.js
@@ -0,0 +1,5 @@
+arr = [1, 2, 3, 4, 5]
+arr2 = []
+for (let i of arr){
+    arr2.push(i)
+}console.log(arr2)
